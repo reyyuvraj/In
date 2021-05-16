9e3eb6bb956140ed58126d6d9dcc893b1c0b4356
reyyuvraj <yuvrajsingh77733999@gmail.com>
2021-05-16 21:05:37 +0530

    Commit

---
+++ app/src/main/java/com/example/ins/recycler/DataStory.kt
@@ -0,0 +1,55 @@
+package com.example.ins.recycler
+
+import com.example.ins.R
+
+class DataStory {
+
+    companion object{
+
+        fun sStory(): ArrayList<ModelStory>{
+
+            val sList = ArrayList<ModelStory>()
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp09)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp08)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp07)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp06)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp07)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp05)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp04)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp03)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp02)
+            )
+            sList.add(
+                ModelStory(
+                    R.drawable.pfp01)
+            )
+            return sList
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ins/recycler/ModelStory.kt
@@ -0,0 +1,3 @@
+package com.example.ins.recycler
+
+data class ModelStory(val sPFP: Int)
